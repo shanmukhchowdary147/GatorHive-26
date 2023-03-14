d0a0e90d9d1e529a879e6f386c216d55f23763e7
Shanmukh Chowdary <57340285+shanmukhchowdary147@users.noreply.github.com>
2023-03-13 23:18:14 -0500

    project with structure

---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,125 @@
+import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import './login.css';
+
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Login form submitted: email=${email}, password=${password}`);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <Form.Group controlId="email">
+        <Form.Label>Email:</Form.Label>
+        <Form.Control
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId="password">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+        />
+      </Form.Group>
+      <Button type="submit">Login</Button>
+      <a href="#" className="ml-2">
+        Forgot Password?
+      </a>
+    </Form>
+  );
+}
+
+function SignupForm() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(
+      `Signup form submitted: firstName=${firstName}, lastName=${lastName}, email=${email}, password=${password}, confirmPassword=${confirmPassword}`
+    );
+  };
+
+  return (
+    <div className="login-container">
+      <Form onSubmit={handleSubmit}>
+        <h2>Sign Up</h2>
+        <Form.Group controlId="firstName">
+          <Form.Label>First Name:</Form.Label>
+          <Form.Control
+            type="text"
+            value={firstName}
+            onChange={(event) => setFirstName(event.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="lastName">
+          <Form.Label>Last Name:</Form.Label>
+          <Form.Control
+            type="text"
+            value={lastName}
+            onChange={(event) => setLastName(event.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="confirmPassword">
+          <Form.Label>Confirm Password:</Form.Label>
+          <Form.Control
+            type="password"
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
+            required
+          />
+        </Form.Group>
+        <Button type="submit">Sign Up</Button>
+      </Form>
+    </div>
+  );
+}
+
+function LoginSignupPage() {
+  return (
+    <div className="login-signup-container">
+      <div className="login-box">
+        <LoginForm />
+      </div>
+      <div className="signup-box">
+        <SignupForm />
+      </div>
+    </div>
+  );
+}
+
+export default LoginSignupPage;
